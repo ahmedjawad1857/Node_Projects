9cac2d2f853ff38d4e51342e9dacb8eac2c0a886
ahmadjawad1857 <ahmedjawad1857@gmail.com>
2024-03-20 23:37:30 +0500

    Project01_Guess the number game

---
+++ guess_the_number_game/index.ts
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber: number = Math.floor(Math.random() * 100 + 1);
+
+let roundsPlayed: number = 10;
+if (roundsPlayed == 10) {
+  console.log(`Welcome to Guess the number Game.`);
+  console.log(
+    `Here I think the number between 1 and 100 in ${roundsPlayed} rounds.`
+  );
+}
+
+async function askToGuess() {
+  if (roundsPlayed >= 0) {
+    const answer = await inquirer.prompt([
+      {
+        type: "number",
+        name: "guess",
+        message: `Guess a number between 1 and 100 in ${roundsPlayed} rounds ->`,
+        validate: (input: string) => {
+          const num = parseFloat(input);
+          if (isNaN(num) || num < 1 || num > 100) {
+            return "Please enter a valid number between 1 to 100.";
+          }
+          return true;
+        },
+      },
+    ]);
+
+    const guess = parseInt(answer.guess);
+    roundsPlayed--;
+    checkGuess(guess);
+  } else {
+    console.log("You lost the number is ", randomNumber);
+    process.exit();
+  }
+}
+
+function checkGuess(guess: number) {
+  if (guess === randomNumber) {
+    console.log("Congrats! You got it in ", 10 - roundsPlayed, " rounds.");
+    process.exit(0);
+  } else if (guess < randomNumber) {
+    console.log("Too low!");
+    askToGuess();
+  } else if (guess > randomNumber) {
+    console.log("Too high!");
+    askToGuess();
+  }
+}
+
+askToGuess();
